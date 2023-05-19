7ebb74ef8c1349236da347ae458dbae98fceb527
wongkj12 <wongkj12@gmail.com>
2023-05-19 18:00:46 +0800

    test

--- wopoat/urls.py
+++ wopoat/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#comment
-#comment 2
+# comment
+# comment 2
+# comment 3
 
 from django.contrib import admin
 from django.urls import path, include  # Add the include method
