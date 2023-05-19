144deef7d7f90007f3f033f2db4c38daa67201fc
unknown <leechewie13@gmail.com>
2023-05-19 17:21:53 +0800

    weichong test commit

--- frontend/src/pages/QuizPage.js
+++ frontend/src/pages/QuizPage.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { MathComponent } from "mathjax-react";
-import { xContentTypeOptions } from "helmet";
 
 function QuizPage() {
   const location = useLocation();
