50fa234a822204dcfba2d10ed966b51425f48f86
wongkj12 <wongkj12@gmail.com>
2023-06-15 00:13:08 +0800

    temp quiz components

--- frontend/src/pages/QuizPage.js
+++ frontend/src/pages/QuizPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Quiz from "../components/Quiz";
 import { useLocation, useNavigate } from "react-router-dom";
 import { MathComponent } from "mathjax-react";
 
