10be2d195477f5a96f388c73d446381023e8ec8c
wongkj12 <wongkj12@gmail.com>
2023-06-21 02:33:31 +0800

    updated about info

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -7,7 +7,13 @@ function AboutPage() {
     <Container maxWidth="md">
       <Paper elevation={3} style={{ marginTop: "2rem", padding: 8 }}>
         <Typography variant="h5">About</Typography>
-        <LoremIpsum />
+        Quizzicalc is an educational tool for quick generation of algebra
+        quizzes. All quizzes are randomly generated, with answers verified using
+        symbolic computation! The platform will feature customisable topics and
+        difficulty, and an account system to view your recent quiz history. Just
+        select your desired topic and difficulty, and start quizzing! <br />
+        <br />
+        Note: Currently, only "Expand" questions are available.
       </Paper>
     </Container>
   );
